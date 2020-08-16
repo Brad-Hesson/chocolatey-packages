563980ae599fbcf5e6d6fa09f3da93089c39e5a8
Brad Hesson <brad.hesson@outlook.com>
2020-08-15 20:27:46 -0600

    put in new package

---
+++ automatic/myki/tools/chocolateyinstall.ps1
@@ -0,0 +1,30 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$url      = 'https://static.myki.com/releases/da/MYKI-latest.exe'
+$checksum = 'f87266298a3e7249cbfd5bfa25e0e17420ca2d4f9b4a5c6681b523cf88c3e63a'
+
+$packageArgs = @{
+    packageName   = $env:ChocolateyPackageName
+    fileType      = 'exe'
+    url           = $url
+    checksum      = $checksum
+    checksumType  = 'sha256'
+    silentArgs    = "-s"
+    validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+Write-Host -NoNewLine 'Waiting for Registry Confirmation.'
+$count = 0
+while((Get-UninstallRegistryKey -SoftwareName "myki*") -eq $null){
+    sleep 1
+    Write-Host -NoNewLine '.'
+    $count += 1
+    if($count -ge 20){
+        Write-Host ''
+        Write-Warning 'Registry confirmation timed-out.  The installation might have failed.'
+        break
+    }
+}
+Write-Host ''
---
+++ automatic/myki/update.ps1
@@ -0,0 +1,23 @@
+﻿Import-Module au
+Import-Module BitsTransfer
+
+$url = 'https://static.myki.com/releases/da/MYKI-latest.exe'
+
+function global:au_GetLatest {
+     Start-BitsTransfer -Source $url -Destination 'temp.7z'
+     $data = (cmd.exe /c 7z l temp.7z)
+     $version = [regex]::match($data,'myki-(?<a>.*)-full.nupkg').groups[1].value
+     Remove-Item 'temp.7z'
+     return @{ Version = $version; URL32 = $url }
+}
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"      #2
+        }
+    }
+}
+
+
+update -checksumfor 32
