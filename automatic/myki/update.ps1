1982df9ae300928cd70e1c9bfe3bc1947d534da9
Brad Hesson <brad.hesson@outlook.com>
2020-09-09 22:11:56 -0600

    changed getLatest method

--- automatic/myki/update.ps1
+++ automatic/myki/update.ps1
@@ -1,20 +1,19 @@
 ﻿Import-Module au
 Import-Module BitsTransfer
 
-$url = 'https://static.myki.com/releases/da/MYKI-latest.exe'
+$url      = 'https://static.myki.com/releases/da/MYKI-latest.exe'
+$releases = 'http://myki-desktop-app.s3.eu-west-1.amazonaws.com'
 
 function global:au_GetLatest {
-     Start-BitsTransfer -Source $url -Destination 'temp.7z'
-     $data = (cmd.exe /c 7z l temp.7z)
-     $version = [regex]::match($data,'myki-(?<a>.*)-full.nupkg').groups[1].value
-     Remove-Item 'temp.7z'
-     return @{ Version = $version; URL32 = $url }
+    $xml = [xml](Invoke-WebRequest -Uri $releases).Content
+    $version = [String]($xml.ListBucketResult.Contents | ? Key -match '-full.nupkg' | % {$_.Key -match '-(.*?)-'|Out-Null; [System.Version]$matches[1]}|sort|Select-Object -Last 1)
+    return @{ Version = $version; URL32 = $url }
 }
 
 function global:au_SearchReplace {
     @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"      #2
+            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
         }
     }
 }
