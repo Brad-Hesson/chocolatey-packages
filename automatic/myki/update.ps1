c5c5b1cfcbd8815b80c5e73e92df9bbf252205b4
Brad-Hesson <brad.hesson@outlook.com>
2021-01-14 14:02:48 -0700

    Force update on checksum mismatch

--- automatic/myki/update.ps1
+++ automatic/myki/update.ps1
@@ -7,6 +7,17 @@ $releases = 'http://myki-desktop-app.s3.eu-west-1.amazonaws.com'
 function global:au_GetLatest {
     $xml = [xml](Invoke-WebRequest -Uri $releases).Content
     $version = [String]($xml.ListBucketResult.Contents | ? Key -match '-full.nupkg' | % {$_.Key -match '-(.*?)-'|Out-Null; [System.Version]$matches[1]}|sort|Select-Object -Last 1)
+
+    $current_checksum = (gi $PSScriptRoot\tools\chocolateyinstall.ps1 | sls '\$checksum *=') -split "=|'" | Select -Last 1 -Skip 1
+    if ($current_checksum.Length -ne 64) { throw "Can't find current checksum" }
+    $remote_checksum  = Get-RemoteChecksum $url
+
+    if ($current_checksum -ne $remote_checksum) {
+        Write-Host 'Remote checksum is different then the current one, forcing update'
+        $global:au_old_force = $global:au_force
+        $global:au_force = $true
+    }
+
     return @{ Version = $version; URL32 = $url }
 }
 
